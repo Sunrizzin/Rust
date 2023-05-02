7bac93aa3b3aa7ef370af60ec8a2bc3ba3c5e9fd
Alexey Usanov <ausanov@broniboy.ru>
2023-05-02 23:44:37 +0700

    add random && loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,28 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("Secret number equal {}", secret_number);
+    loop {
     println!("Please, enter your number");
 
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Error for read string!");
 
-    println!("You enter: {}", guess)
+    let guess: u32 = guess.trim().parse().expect("Please, enter number!");
+    println!("You enter: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Number is so small!"),
+        Ordering::Greater => println!("Number is so bug!"),
+        Ordering::Equal => {
+            println!("You win!");
+            break;
+        }
+    }
+}
 }
