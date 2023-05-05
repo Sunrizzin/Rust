ac19c6e989a5b881ab6f697e78f2b9f547b22150
Алексей Усанов <sunrizz@MacBook-Sunrizz.local>
2023-05-05 08:50:41 +0700

    add const

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,8 @@ fn main() {
     println!("Number x equals  {}", x);
     x = 6;
     println!("Number x equals {}", x);
+
+    const MAX_POINTS: i32 = 1_000_000;
+
+    println!("MAX POINTS equals {}", MAX_POINTS);
 }
