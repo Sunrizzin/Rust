2724a94da0d886c45c986c32b4eac73ca4724fdd
Алексей Усанов <sunrizz@MacBook-Sunrizz.local>
2023-05-07 21:29:21 +0700

    add links example

--- links/src/main.rs
+++ links/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("Hello");
+    let len = calculate_lenght(&s1);
+    println!("Lenght '{}' equals {}", s1, len)
+}
+
+fn calculate_lenght(s: &String) -> usize {
+    s.len()
 }
