ac8b043091988ed5477c8d0be15cafd3a833c25a
Алексей Усанов <sunrizz@MacBook-Sunrizz.local>
2023-05-07 18:19:24 +0700

    add example

--- visible/src/main.rs
+++ visible/src/main.rs
@@ -1,6 +1,17 @@
 fn main() {
     let mut s = String::from("hello");
     s.push_str(", world!");
-
     println!("{}", s);
+
+/* Example howto visible works
+    let s1 = String::from("Alex");
+    let s2 = s1;
+    println!("{}", s1);
+*/
+/* Example howto clone works
+    let s1 = String::from("Alex");
+    let mut s2 = s1.clone();
+    s2.push_str(" Usanov");
+    println!("{} | {}", s1, s2);
+*/
 }
