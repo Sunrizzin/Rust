d3d1cfb3b52ca69334cb4b2fdc359458a2c53dd5
Алексей Усанов <sunrizz@MacBook-Sunrizz.local>
2023-05-07 18:41:41 +0700

    visible from fn

--- visible/src/main.rs
+++ visible/src/main.rs
@@ -14,4 +14,20 @@ fn main() {
     s2.push_str(" Usanov");
     println!("{} | {}", s1, s2);
 */
+/* Example visible from fn
+    let r = String::from("hello");
+
+    takes_ownership(r);
+    println!("{}", r);
+    let x = 5;
+    makes_copy(5);
+*/
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string)
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer)
 }
